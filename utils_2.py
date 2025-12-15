a1059ce65db902370ff11534993ee4d16d0ac7b6
Jennifer Zhao <jennifer-zhao@berkeley.edu>
2025-12-15 13:13:17 -0800

    ran simulations

--- utils_2.py
+++ utils_2.py
@@ -8,10 +8,33 @@ VOTERS = []
 CANDIDATES = np.arange(NUM_CANDIDATES)
 
 
-def generate_random_voters(num_voters, num_candidates):
+def generate_uniform_random_voters(num_voters, num_candidates):
     voters = []
     for _ in range(num_voters):
+        # if ut_distr == 'uniform':
         vec = np.random.random(num_candidates)
+        # elif ut_distr == 'normal':
+        #     vec = np.random.normal(0.5, 0.25, size=num_candidates)
+        #     vec = np.clip(vec, 0.0, 1.0)
+        ut_func = (lambda v: lambda c: v[c])(vec)
+        voters.append(ut_func)
+    return voters
+
+def generate_normal_random_voters(num_voters, num_candidates, mean=0.5, std=0.15):
+    voters = []
+    for _ in range(num_voters):
+        vec = np.random.normal(mean, std, size=num_candidates)
+        vec = np.clip(vec, 0.0, 1.0)
+        ut_func = (lambda v: lambda c: v[c])(vec)
+        voters.append(ut_func)
+    return voters
+
+def generate_agreement_random_voters(num_voters, num_candidates, agreement=0.5):
+    underlying = np.random.random(num_candidates)
+    voters = []
+    for _ in range(num_voters):
+        vec = [np.random.normal(underlying[i], agreement) for i in range(num_candidates)]
+        vec = np.clip(vec, 0.0, 1.0)
         ut_func = (lambda v: lambda c: v[c])(vec)
         voters.append(ut_func)
     return voters
@@ -63,7 +86,7 @@ def winner_peeling_ranking(voters, candidates, voters_distr=None, candidates_dis
 
         remaining_idxs = (candidates != winner)
         candidates = candidates[remaining_idxs]
-        if candidates_distr:
+        if candidates_distr is not None:
             candidates_distr = candidates_distr[remaining_idxs]
 
     return np.array(wpr)
@@ -74,37 +97,74 @@ def distortion_with_weights(voters, candidates, your_ranking, weights, voters_di
 
     tr = true_ranking(voters=voters, candidates=candidates, voters_distr=voters_distr)
 
-    if weights is not None:
-        avg_utils = avg_util(voters=voters, candidates=candidates, voters_distr=voters_distr)
-        numerator = avg_utils[tr] @ weights
-        denominator = avg_utils[your_ranking] @ weights
-        return numerator / denominator
+    avg_utils = avg_util(voters=voters, candidates=candidates, voters_distr=voters_distr)
+    numerator = avg_utils[tr] @ weights
+    denominator = avg_utils[your_ranking] @ weights
+    return numerator / denominator
     
-    else:
-        def f(deltas, tr, yr):
-            n = len(deltas)
-            weights = np.zeros(n)
-            weights[-1] = deltas[-1]
+def distortion(voters, candidates, your_ranking, voters_distr=None, return_weights=False):
+    if voters_distr is None:
+        voters_distr = np.array([1.0 / len(voters) for v in voters])
 
-            # build backwards
-            for i in range(n - 2, -1, -1):
-                weights[i] = weights[i + 1] + deltas[i]
-            value = (avg_utils[tr] @ weights) / (avg_utils[yr] @ weights)
+    tr = true_ranking(voters=voters, candidates=candidates, voters_distr=voters_distr)
+    avg_utils = avg_util(voters=voters, candidates=candidates, voters_distr=voters_distr)
 
-            return value  # scalar
-        
-        def objective(deltas):
-            return -f(deltas, tr, your_ranking)
-        
-        res = minimize(
-            fun=objective,
-            x0=np.ones(len(tr)),
-            bounds=[(0, None)] * len(tr),
-            method="L-BFGS-B"
-        )
+    u_tr = avg_utils[tr]
+    u_yr = avg_utils[your_ranking]
+
+    prefix_tr = np.cumsum(u_tr)
+    prefix_yr = np.cumsum(u_yr)
+
+    ratios = prefix_tr / prefix_yr
+    k_star = np.argmax(ratios)
+
+    distortion = ratios[k_star]
+
+    weights = np.zeros(len(tr))
+    weights[:k_star + 1] = 1 / (k_star + 1)
+
+
+    # def f(deltas, tr, yr):
+    #     n = len(deltas)
+    #     weights = np.zeros(n)
+    #     weights[-1] = deltas[-1]
+
+    #     # build backwards
+    #     for i in range(n - 2, -1, -1):
+    #         weights[i] = weights[i + 1] + deltas[i]
+    #     weights = np.array(weights)
+    #     weights /= weights.sum()
+    #     value = (avg_utils[tr] @ weights) / (avg_utils[yr] @ weights)
+
+    #     return value  # scalar
+    
+    # def objective(deltas):
+    #     return -f(deltas, tr, your_ranking)
+    
+    # res = minimize(
+    #     fun=objective,
+    #     x0=np.ones(len(tr)),
+    #     bounds=[(0, None)] * len(tr),
+    #     method="L-BFGS-B"
+    # )
+    # # delta_opt = res.x
+    # distortion = -res.fun
+
+    if return_weights:
         # delta_opt = res.x
-        distortion = -res.fun
-        return distortion
+
+        # n = len(delta_opt)
+        # weights = np.zeros(n)
+        # weights[-1] = delta_opt[-1]
+
+        # # build backwards
+        # for i in range(n - 2, -1, -1):
+        #     weights[i] = weights[i + 1] + delta_opt[i]
+        # weights = np.array(weights)
+        # weights /= weights.sum()
+        return distortion, weights
+
+    return distortion
     
 def plot_utility_curves(voters, candidates):
     plt.figure(figsize=(10, 6))
